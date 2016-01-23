1f353b6e8c16b520cbde1be0d62d78d94fb8f0d9
Hassan Ali <helfi92@gmail.com>
2016-01-23 14:00:22 -0500

    profileview and profileController code

--- www/js/profileController.js
+++ www/js/profileController.js
@@ -1,3 +1,13 @@
-ctrlApp.controller('profileCtrl', function($scope) {
+var appCtrl = angular.module('app.controllers');
+
+appCtrl.controller('profileCtrl', function($scope) {
 	console.log('profilePage');
+
+	$scope.user = {
+
+		firstName : 'Hassan',
+		profession: 'Web Developer',
+		location : 'Brossard, Quebec, Canada',
+		skills : ['Javascript','AngularJS','Html5','CSS3','NodeJS']
+	};
 })
