c6438a026fe25b9b108b4b0685ec6c805e364b13
David Herrera <davidfherrerar@gmail.com>
2016-01-23 11:59:16 -0500

    sass compiler

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,27 @@
 angular.module('app.controllers', [])
-  
+
 .controller('cameraTabDefaultPageCtrl', function($scope) {
 
 })
-   
+
 .controller('cartTabDefaultPageCtrl', function($scope) {
 
 })
-   
+
 .controller('cloudTabDefaultPageCtrl', function($scope) {
 
 })
-      
-.controller('loginCtrl', function($scope) {
+
+.controller('loginCtrl', function($scope,$http) {
+  $scope.signin=function()
+  {
+    $http.get('https://www.linkedin.com/uas/oauth2/authorization',function(response){
+      console.log(response);
+    });
+  };
 
 })
-   
+
 .controller('nameOfPersonCtrl', function($scope) {
 
 })
- 
