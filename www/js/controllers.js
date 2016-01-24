bcfd21ac5b02692eb895992f251f65f723843392
David Herrera <davidfherrerar@gmail.com>
2016-01-23 23:00:10 -0500

    disable login

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,6 +10,7 @@ angular.module('app.controllers', [])
 
 .controller('loginCtrl', function($scope,$http,$rootScope,$location) {
   //window.open('https://www.linkedin.com/uas/oauth2/authorization?client_id=771u0p20ttoai5&…100%2F&response_type=code&scope=r_basicprofile&state=DCEeFWf45A53sdfKef424');
+  $location.path('#/page1/page2');
   $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
     var accessToken=window.localStorage.getItem('AccessToken');
     console.log(accessToken)
